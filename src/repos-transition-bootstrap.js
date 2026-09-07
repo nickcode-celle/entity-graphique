3e3751555337193648a5f898d12c018d83d6a7f1
nickcode-celle <nick.virion@gmail.com>
2026-09-07 19:36:50 +0200

    Expose REPOS serial targets for transition

---
+++ src/repos-transition-bootstrap.js
@@ -0,0 +1,57 @@
+import * as THREE from 'three'
+
+const SERIAL_COUNT=500
+let writeIndex=0,batches=0
+const serialPositions=Array.from({length:SERIAL_COUNT},()=>new THREE.Vector3())
+let capturedCamera=null
+
+const originalSetMatrixAt=THREE.InstancedMesh.prototype.setMatrixAt
+THREE.InstancedMesh.prototype.setMatrixAt=function(index,matrix){
+  const e=matrix.elements
+  serialPositions[writeIndex].set(e[12],e[13],e[14])
+  writeIndex=(writeIndex+1)%SERIAL_COUNT
+  if(writeIndex===0)batches++
+  return originalSetMatrixAt.call(this,index,matrix)
+}
+
+const originalRender=THREE.WebGLRenderer.prototype.render
+THREE.WebGLRenderer.prototype.render=function(scene,camera){
+  if(!capturedCamera)capturedCamera=camera
+  return originalRender.call(this,scene,camera)
+}
+
+await import('./main-repos-transition-300.js')
+THREE.WebGLRenderer.prototype.render=originalRender
+
+function forceValidatedRepos(){
+  const wanted={SEPARATION:'40',ALIGNEMENT:'1',COHESION:'60',VITESSE:'0.35'}
+  let changed=0
+  for(const row of document.querySelectorAll('.controller')){
+    const name=row.querySelector('.name')?.textContent.trim()
+    if(!(name in wanted))continue
+    const input=row.querySelector('input')
+    if(!input)continue
+    input.value=wanted[name]
+    input.dispatchEvent(new Event('input',{bubbles:true}))
+    input.dispatchEvent(new Event('change',{bubbles:true}))
+    changed++
+  }
+  return changed===4
+}
+let tries=0
+const timer=setInterval(()=>{tries++;if(forceValidatedRepos()||tries>100)clearInterval(timer)},50)
+
+function expose(){
+  if(capturedCamera&&batches>=2){
+    window.reposTransitionAPI={
+      getTarget(serialId){
+        const i=((serialId%SERIAL_COUNT)+SERIAL_COUNT)%SERIAL_COUNT
+        const p=serialPositions[i].clone().project(capturedCamera)
+        return {serialId:i,x:p.x,y:p.y,z:p.z}
+      },
+      state(){return {batches}}
+    }
+    window.parent.postMessage({type:'REPOS_TRANSITION_READY'},'*')
+  }else setTimeout(expose,50)
+}
+expose()
